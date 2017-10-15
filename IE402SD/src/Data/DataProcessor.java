cceae70412c45c913098bd41b3053d0552bc1a53
Arda <avedis.ozgul@ozu.edu.tr>
2017-10-15 20:28:58 +0300

    devam işte

--- IE402SD/src/Data/DataProcessor.java
+++ IE402SD/src/Data/DataProcessor.java
@@ -5,25 +5,80 @@ import java.util.Scanner;
 
 public class DataProcessor {
 	private ArrayList<String> dataRaw;
+	private int truckSize;
 	
 	public DataProcessor(ArrayList<String> dataRaw) {
 		this.dataRaw = dataRaw;
 	}
 	
 	public int getTruckSize() {
-		int truckSize = 0;
+		int tSize = 0;
 		Scanner sc = new Scanner(dataRaw.get(0));
 		sc.useDelimiter("=");
 		while(sc.hasNext()) {
 			if (sc.hasNextInt()) {
-				truckSize = sc.nextInt();
+				tSize = sc.nextInt();
 			} else 
 				sc.next();
 		}
 		sc.close();
-		return truckSize;
+		return tSize;
 	}
 	
+	public int getNumberOfOrders() {
+		int noOfOrders = 0;
+		Scanner sc = new Scanner(dataRaw.get(1));
+		sc.useDelimiter("=");
+		while(sc.hasNext()) {
+			if (sc.hasNextInt()) {
+				noOfOrders = sc.nextInt();
+			} else 
+				sc.next();
+		}
+		sc.close();
+		return noOfOrders;
+	}
 	
+	public int getNumberOfNodes() {
+		int noOfNodes = 0;
+		Scanner sc = new Scanner(dataRaw.get(2));
+		sc.useDelimiter("=");
+		while(sc.hasNext()) {
+			if (sc.hasNextInt()) {
+				noOfNodes = sc.nextInt();
+			} else 
+				sc.next();
+		}
+		sc.close();
+		return noOfNodes;
+	}
+	
+	public int getNumberOfRoutes() {
+		int noOfRoutes = 0;
+		Scanner sc = new Scanner(dataRaw.get(3));
+		sc.useDelimiter("=");
+		while(sc.hasNext()) {
+			if (sc.hasNextInt()) {
+				noOfRoutes = sc.nextInt();
+			} else 
+				sc.next();
+		}
+		sc.close();
+		return noOfRoutes;
+	}
+	
+	public double[] getOrderSize() {
+		double[] orderSize = new double[getNumberOfOrders()];
+		Scanner sc = new Scanner(dataRaw.get(4));
+		sc.useDelimiter("=");
+		sc.next();
+		sc.useDelimiter(",");
+		int i=0;
+		while(sc.hasNext()) {
+			orderSize[i] = sc.nextDouble();
+			i++;
+		}
+		return orderSize;
+	}
 	
 }
